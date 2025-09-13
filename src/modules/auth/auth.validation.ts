6a4a0713ae70199c0d8cebe2df0f05cc8e4b0f75
mohamed <mohammedeldesoky77@gmail.com>
2025-09-13 17:39:21 +0300

    feat : install zod and implement register-schema-validation

---
+++ src/modules/auth/auth.validation.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { GENDER_TYPES, USER_AGENT } from "../../utils/common/enum";
+import { RegistterDTO } from "./auth.dto";
+
+// register validaton  schema
+// all fields by default are required
+export const registerSchema = z.object<RegistterDTO>({
+  fullName: z.string().min(3).max(20) as unknown as string, //TYPE assertion
+  email: z.email() as unknown as string,
+  password:z.string() as unknown as string,
+  phoneNumber: z.string().min(11).max(11) as unknown as string,
+  gender:z.enum(GENDER_TYPES) as unknown as GENDER_TYPES,
+
+});
